a27a2799634469e26acbdc910ddb38d764f0a915
doctype-Melvin <doctype.melvin@gmail.com>
2023-01-04 09:30:57 +0100

    initial shop setup

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,14 +1,33 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import Profile from "./Profile";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
 
 const RouteSwitch = () => {
     return (
         <BrowserRouter>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/shopping-cart/components/Home">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/shopping-cart/components/Shop">Shop</Link>
+                        </li>
+                        <li>
+                            <Link to="/shopping-cart/components/Cart">Cart</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="./src/profile" element={<Profile />} />
+            <Route path="/shopping-cart/" element={<Home />} />
+            <Route path="/shopping-cart/components/Home" element={<Home />} />
+            <Route path="/shopping-cart/components/Shop" element={<Shop />} />
+            <Route path="/shopping-cart/components/Cart" element={<Cart />} />
         </Routes>
         </BrowserRouter>
     )
---
+++ src/tests/App.test.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import  userEvent from "@testing-library/user-event";
+import App from "../App";
+
