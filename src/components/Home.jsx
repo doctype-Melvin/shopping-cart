7b35ab2a5f7c65947ae386cc171846112db6b076
doctype-Melvin <doctype.melvin@gmail.com>
2023-01-10 10:06:20 +0100

    source path of images set to base

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,9 +10,9 @@ const Home = (props) => {
             <h1 className="title">Aphrodite, Venus & Helen</h1>
             <p className="welcome">Welcome to the world of beauty and spa.</p>
             <div className="preview">
-                <NavLink to="/shopping-cart/components/Shop" className="shopLink"><img className="prevImg" src="pic4.jpeg"></img></NavLink>
-                <NavLink to="/shopping-cart/components/Shop" className="shopLink"><img className="prevImg" src="pic3.jpeg"></img></NavLink>
-                <NavLink to="/shopping-cart/components/Shop" className="shopLink"><img className="prevImg" src="pic6.jpeg"></img></NavLink>
+                <NavLink to="/shopping-cart/components/Shop" className="shopLink"><img className="prevImg" src="/shopping-cart/pic4.jpeg"></img></NavLink>
+                <NavLink to="/shopping-cart/components/Shop" className="shopLink"><img className="prevImg" src="/shopping-cart/pic3.jpeg"></img></NavLink>
+                <NavLink to="/shopping-cart/components/Shop" className="shopLink"><img className="prevImg" src="/shopping-cart/pic6.jpeg"></img></NavLink>
             </div>
         </div>
     )
